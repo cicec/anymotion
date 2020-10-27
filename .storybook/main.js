eaa311a895fbe99a76255b65d55310f006ea5b40
clancysong <clancysong@gmail.com>
2020-10-27 11:02:24 +0800

    ➕ Add sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,15 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.s[ac]ss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    })
+
+    return config
+  },
 }
